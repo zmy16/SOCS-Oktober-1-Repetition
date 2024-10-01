f2912b6c0d8ffb6a92336473f49b6d5354f64f00
Muhammad Raid Zakwan <90046024+zmy16@users.noreply.github.com>
2024-10-01 21:11:08 +0700

    Update AE.c

--- AE.c
+++ AE.c
@@ -11,13 +11,10 @@ int main() {
     for(int i = 0; i < T; i++){
         scanf("%d %d", &R, &H);
 
-        // Menghitung luas permukaan dan menyimpannya dalam array
         area[i] = 2 * Pi * R * (R + H);
     }
 
-    // Loop untuk mencetak semua hasil di akhir
     for(int i = 0; i < T; i++){
-        // Output sesuai dengan format yang diminta
         printf("Case #%d: %.2f\n", i+1, area[i]);
     }
 
