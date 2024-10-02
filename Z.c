9d1b01910c8a74065460b416b2d130bb0a473722
Muhammad Raid Zakwan <90046024+zmy16@users.noreply.github.com>
2024-10-02 23:07:37 +0700

    Update Z.c

--- Z.c
+++ Z.c
@@ -7,22 +7,21 @@ int main() {
     int posisi = 0; 
 
     for (int i = 0; i < N; i++) {
-        int ki; // Jumlah lemparan dadu 
+        int ki; 
         scanf("%d", &ki);
 
         posisi += ki;
 
-        // Jika posisi melebihi atau sama dengan 40, kembali ke awal
         if (posisi >= 40) {
             posisi -= 40;
         }
 
         if (posisi == 30) {
-            posisi = 10; // Pindahkan ke petak penjara
+            posisi = 10;
         } else if (posisi == 12) {
-            posisi = 28; // Pindahkan ke petak 28
+            posisi = 28;
         } else if (posisi == 35) {
-            posisi = 7; // Pindahkan ke petak 7
+            posisi = 7;
         }
     }
 
